f18bcb5ce39c21515c14b81904ce840a0a2ae090
Raxomygod <etienne.sarrail95@gmail.com>
2022-11-08 18:03:29 +0100

    punch section ok tier + started customers reviews section

--- script.js
+++ script.js
@@ -1,14 +1,33 @@
 const productPresentation = document.getElementsByClassName(
   "product-presentation"
 );
+
 const getStarted = document.getElementsByClassName("get-started-btn");
 const purchaseContainer = document.getElementsByClassName("purchase-container");
-console.log(getStarted);
+const purchaseBtn = document.getElementsByClassName("purchase-btn");
+const purchaseCloseBtn = document.getElementsByClassName(
+  "purchase-container-close-btn"
+);
 
 getStarted[0].addEventListener("click", () => {
   if (purchaseContainer[0].style.display === "flex") {
     purchaseContainer[0].style.display = "none";
   } else {
     purchaseContainer[0].style.display = "flex";
+  }
+});
+
+purchaseCloseBtn[0].addEventListener("click", () => {
+  purchaseContainer[0].style.display = "none";
+});
+
+let userMonthSelection;
+purchaseBtn[0].addEventListener("click", () => {
+  if (document.getElementById("month").checked) {
+    userMonthSelection = 1;
+  } else if (document.getElementById("months-6").checked) {
+    userMonthSelection = 6;
+  } else if (document.getElementById("months-12").checked) {
+    userMonthSelection = 12;
   }
 });
